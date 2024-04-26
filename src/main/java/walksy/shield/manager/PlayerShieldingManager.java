5e577c92189f210236a5ecae5c9b57252091cb30
Walksy <walksylol@gmail.com>
2024-04-26 17:06:59 +0100

    Code Refactor - Fixed All Animation Bugs

--- src/main/java/walksy/shield/manager/PlayerShieldingManager.java
+++ src/main/java/walksy/shield/manager/PlayerShieldingManager.java
@@ -1,134 +1,85 @@
 package walksy.shield.manager;
 
-import com.mojang.datafixers.util.Pair;
-import net.minecraft.block.entity.BannerBlockEntity;
-import net.minecraft.block.entity.BannerPattern;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.client.model.ModelPart;
 import net.minecraft.client.network.AbstractClientPlayerEntity;
 import net.minecraft.client.network.ClientPlayerEntity;
-import net.minecraft.client.render.VertexConsumer;
-import net.minecraft.client.render.VertexConsumerProvider;
-import net.minecraft.client.render.block.entity.BannerBlockEntityRenderer;
-import net.minecraft.client.render.entity.PlayerEntityRenderer;
-import net.minecraft.client.render.entity.model.*;
-import net.minecraft.client.render.item.ItemRenderer;
-import net.minecraft.client.render.model.ModelLoader;
-import net.minecraft.client.render.model.json.ModelTransformationMode;
-import net.minecraft.client.util.SpriteIdentifier;
-import net.minecraft.client.util.math.MatrixStack;
+import net.minecraft.client.render.entity.model.BipedEntityModel;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
-import net.minecraft.entity.damage.DamageSource;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.item.*;
-import net.minecraft.registry.entry.RegistryEntry;
+import net.minecraft.item.CrossbowItem;
+import net.minecraft.item.ItemStack;
+import net.minecraft.item.Items;
 import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvent;
 import net.minecraft.sound.SoundEvents;
 import net.minecraft.text.Text;
-import net.minecraft.util.Arm;
-import net.minecraft.util.DyeColor;
 import net.minecraft.util.Hand;
 import net.minecraft.util.UseAction;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.MathHelper;
-import net.minecraft.util.math.RotationAxis;
 import net.minecraft.util.math.Vec3d;
 import net.minecraft.world.World;
-import org.joml.*;
 import org.joml.Math;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-import walksy.shield.main.ShieldFixMod;
+import walksy.shield.main.ILivingEntity;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 public class PlayerShieldingManager {
 
-    public ArrayList<ShieldingPlayer> shieldingPlayers = new ArrayList<>();
-    public ArrayList<DisabledShieldPlayer> disabledShieldPlayers = new ArrayList<>();
+    public static PlayerShieldingManager INSTANCE = new PlayerShieldingManager();
+    private final Map<Entity, Integer> entityUseTimeMap = new HashMap<>();
 
-    /**
-     * This code uses the #usingShield(LivingEntity) method to collect an arraylist of players shielding
-     * However they're not actually shielding due to it not factoring in the 5 tick delay
-     * So we factor this in ourselves (See #ShieldingPlayer)
-     */
     public void tick() {
-        shieldingPlayers.removeIf(shieldingPlayer -> !usingShield(shieldingPlayer.getPlayer()));
-        shieldingPlayers.forEach(ShieldingPlayer::tick);
-        disabledShieldPlayers.removeIf(disabledShieldPlayer -> disabledShieldPlayer.disabledTime <= 0);
-        disabledShieldPlayers.forEach(DisabledShieldPlayer::tick);
+        MinecraftClient.getInstance().world.getEntities().forEach(entity -> {
 
-        MinecraftClient.getInstance().world.getPlayers().forEach(player -> {
-            if (usingShield(player) && player != MinecraftClient.getInstance().player) {
-                boolean playerAlreadyExists = false;
-                for (ShieldingPlayer shieldingPlayer : shieldingPlayers) {
-                    if (shieldingPlayer.getPlayer() == player) {
-                        playerAlreadyExists = true;
-                        break;
-                    }
+            if (entity instanceof PlayerEntity player && player != MinecraftClient.getInstance().player) {
+                int useTime = entityUseTimeMap.getOrDefault(entity, 72000); //create an integer map with each instance of a player
+
+                if (player.isUsingItem() && isHoldingUsableShield(player)) {
+                    ((ILivingEntity) player).setActiveItem(player.getStackInHand(player.getActiveHand()));
+                    ((ILivingEntity) player).setItemUseTime(useTime);
+                    useTime--;
+                } else {
+                    useTime = 72000;
                 }
-                if (!playerAlreadyExists) {
-                    shieldingPlayers.add(new ShieldingPlayer(player));
-                }
+
+                entityUseTimeMap.put(entity, useTime);
             }
         });
     }
 
-
-    public void handleByteStatus(byte status, Object castedClass)
+    public void onAttack()
     {
-        LivingEntity entity = LivingEntity.class.cast(castedClass);
-        switch (status)
-        {
-            case 30 -> { //Disabled Shield
-                disabledShieldPlayers.add(new DisabledShieldPlayer(entity));
-                MinecraftClient.getInstance().world.playSound(
-                    entity.getX(),
-                    entity.getY(),
-                    entity.getZ(),
-                    SoundEvents.ITEM_SHIELD_BREAK,
-                    SoundCategory.PLAYERS,
-                    1F,
-                    0.8F + MinecraftClient.getInstance().world.random.nextFloat() * 0.4F,
-                    false
-                );
-            }
-        }
-    }
+        PlayerEntity me = MinecraftClient.getInstance().player;
 
-
-    public void onAttackEntity()
-    {
-        PlayerEntity player = MinecraftClient.getInstance().player;
-
-        if (player.disablesShield()) return;
-        ItemStack itemStack = player.getStackInHand(Hand.MAIN_HAND);
+        if (me.disablesShield()) return; //if holding an axe, don't continue - We use the entityByteStatus to play shield disable sounds
+        ItemStack itemStack = me.getStackInHand(Hand.MAIN_HAND); //can only attack with mainHand (duh?)
         if (!itemStack.isItemEnabled(MinecraftClient.getInstance().world.getEnabledFeatures())) return;
-        if (MinecraftClient.getInstance().crosshairTarget instanceof EntityHitResult hitResult) {
-            shieldingPlayers.forEach(shieldingPlayer ->
-            {
-                if (shieldingPlayer.actuallyShielding() && hitResult.getEntity() == shieldingPlayer.getPlayer()) {
-                    Vec3d rotation = shieldingPlayer.getPlayer().getRotationVec(1);
-                    Vec3d relativePosition = player.getPos().relativize(shieldingPlayer.getPlayer().getPos()).normalize();
-                    Vec3d flat = new Vec3d(relativePosition.x, 0.0, relativePosition.z);
-                    if (flat.dotProduct(rotation) < 0.0) {
-                        MinecraftClient.getInstance().world.playSound(
-                            player.getX(),
-                            player.getY(),
-                            player.getZ(),
-                            SoundEvents.ITEM_SHIELD_BLOCK,
-                            SoundCategory.PLAYERS,
-                            1F,
-                            0.8F + MinecraftClient.getInstance().world.random.nextFloat() * 0.4F,
-                            false
-                        );
-                    }
+
+        if (MinecraftClient.getInstance().crosshairTarget instanceof EntityHitResult hitResult && hitResult.getEntity() instanceof PlayerEntity target)
+        {
+            if (target.isBlocking()) {
+                Vec3d rotation = target.getRotationVec(1);
+                Vec3d relativePosition = me.getPos().relativize(target.getPos()).normalize();
+                Vec3d flat = new Vec3d(relativePosition.x, 0.0, relativePosition.z);
+                if (flat.dotProduct(rotation) < 0.0) {
+                    MinecraftClient.getInstance().world.playSound(
+                        target.getX(),
+                        target.getY(),
+                        target.getZ(),
+                        SoundEvents.ITEM_SHIELD_BLOCK,
+                        SoundCategory.PLAYERS,
+                        1F,
+                        0.8F + MinecraftClient.getInstance().world.random.nextFloat() * 0.4F,
+                        false
+                    );
                 }
-            });
+            }
         }
     }
 
@@ -152,115 +103,106 @@ public class PlayerShieldingManager {
             if (nearEntity.isAlive()) {
                 double distance = Math.sqrt(nearEntity.squaredDistanceTo(pos));
                 if (distance < maxDistance) {
-                    shieldingPlayers.forEach(shieldingPlayer -> {
-                        if (shieldingPlayer.getPlayer() != nearEntity) return;
-                        Vec3d rotation = shieldingPlayer.getPlayer().getRotationVec(1);
-                        Vec3d relativePosition = pos.relativize(shieldingPlayer.getPlayer().getPos()).normalize();
-                        Vec3d flat = new Vec3d(relativePosition.x, 0.0, relativePosition.z);
-                        if (flat.dotProduct(rotation) < 0.0) {
-                            if (shieldingPlayer.actuallyShielding()) {
-                                world.playSound(
-                                    nearEntity.getX(),
-                                    nearEntity.getY(),
-                                    nearEntity.getZ(),
-                                    SoundEvents.ITEM_SHIELD_BLOCK,
-                                    nearEntity.getSoundCategory(),
-                                    1F,
-                                    0.8F + MinecraftClient.getInstance().world.random.nextFloat() * 0.4F,
-                                    false
-                                );
-                            }
+                    Vec3d rotation = nearEntity.getRotationVec(1);
+                    Vec3d relativePosition = pos.relativize(nearEntity.getPos()).normalize();
+                    Vec3d flat = new Vec3d(relativePosition.x, 0.0, relativePosition.z);
+                    if (flat.dotProduct(rotation) < 0.0) {
+                        if (nearEntity.isBlocking()) {
+                            world.playSound(
+                                nearEntity.getX(),
+                                nearEntity.getY(),
+                                nearEntity.getZ(),
+                                SoundEvents.ITEM_SHIELD_BLOCK,
+                                nearEntity.getSoundCategory(),
+                                1F,
+                                0.8F + MinecraftClient.getInstance().world.random.nextFloat() * 0.4F,
+                                false
+                            );
                         }
-                    });
+                    }
                 }
             }
         }
     }
 
-    public void setArmPose(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir) {
+    public void handleByteStatus(byte status, Object castedClass)
+    {
+        LivingEntity entity = LivingEntity.class.cast(castedClass);
+        if (status == 30) {//Disabled Shield
+            MinecraftClient.getInstance().world.playSound(
+                    entity.getX(),
+                    entity.getY(),
+                    entity.getZ(),
+                    SoundEvents.ITEM_SHIELD_BREAK,
+                    SoundCategory.PLAYERS,
+                    1F,
+                    0.8F + MinecraftClient.getInstance().world.random.nextFloat() * 0.4F,
+                    false
+            );
+        }
+    }
+
+    public void setArmPose(AbstractClientPlayerEntity player, Hand hand, CallbackInfoReturnable<BipedEntityModel.ArmPose> cir)
+    {
+        if (player == MinecraftClient.getInstance().player) return;
         cir.cancel();
         ItemStack itemStack = player.getStackInHand(hand);
         if (itemStack.isEmpty()) {
             cir.setReturnValue(BipedEntityModel.ArmPose.EMPTY);
         } else {
-            if (itemStack.isOf(Items.SHIELD)) {
-                for (ShieldingPlayer shieldingPlayer : ShieldFixMod.getShieldingManager().shieldingPlayers) {
-                    if (shieldingPlayer.actuallyShielding()) {
-                        if (shieldingPlayer.getPlayer() == player) {
-                            cir.setReturnValue(BipedEntityModel.ArmPose.BLOCK);
-                            return;
-                        }
-                    } else {
-                        cir.setReturnValue(BipedEntityModel.ArmPose.ITEM);
-                        return;
-                    }
-                }
-            }
             if (player.getActiveHand() == hand && player.getItemUseTimeLeft() > 0) {
                 UseAction useAction = itemStack.getUseAction();
-
-                if (player == MinecraftClient.getInstance().player)
-                {
-                    if (useAction == UseAction.BLOCK)
-                    {
-                        cir.setReturnValue(BipedEntityModel.ArmPose.BLOCK);
-                        return;
-                    }
+                if (useAction == UseAction.BLOCK && player.isBlocking()) {
+                    cir.setReturnValue(BipedEntityModel.ArmPose.BLOCK);
+                    return;
                 }
 
                 if (useAction == UseAction.BOW) {
                     cir.setReturnValue(BipedEntityModel.ArmPose.BOW_AND_ARROW);
                     return;
                 }
+
                 if (useAction == UseAction.SPEAR) {
                     cir.setReturnValue(BipedEntityModel.ArmPose.THROW_SPEAR);
                     return;
                 }
+
                 if (useAction == UseAction.CROSSBOW && hand == player.getActiveHand()) {
                     cir.setReturnValue(BipedEntityModel.ArmPose.CROSSBOW_CHARGE);
                     return;
                 }
+
                 if (useAction == UseAction.SPYGLASS) {
                     cir.setReturnValue(BipedEntityModel.ArmPose.SPYGLASS);
                     return;
                 }
+
                 if (useAction == UseAction.TOOT_HORN) {
                     cir.setReturnValue(BipedEntityModel.ArmPose.TOOT_HORN);
                     return;
                 }
+
                 if (useAction == UseAction.BRUSH) {
                     cir.setReturnValue(BipedEntityModel.ArmPose.BRUSH);
                     return;
                 }
-            } else if (!player.handSwinging && itemStack.isOf(Items.CROSSBOW) && CrossbowItem.isCharged(itemStack)) {
+            } else if (!player.handSwinging && itemStack.isOf(Items.CROSSBOW) && CrossbowItem.isCharged(itemStack) && !player.isBlocking()) {
                 cir.setReturnValue(BipedEntityModel.ArmPose.CROSSBOW_HOLD);
                 return;
             }
+
             cir.setReturnValue(BipedEntityModel.ArmPose.ITEM);
         }
     }
 
-
-    public boolean isHoldingShield(LivingEntity entity)
+    public boolean isHoldingUsableShield(LivingEntity entity)
     {
-        return entity.getMainHandStack().isOf(Items.SHIELD) || entity.getOffHandStack().isOf(Items.SHIELD);
+        return (entity.getMainHandStack().isOf(Items.SHIELD) || entity.getOffHandStack().isOf(Items.SHIELD)) && !isHoldingAnimationItemMainHand(entity);
     }
 
-    public boolean isHoldingAnimationItemMainHand(LivingEntity entity)
+    private boolean isHoldingAnimationItemMainHand(LivingEntity entity)
     {
-        return entity.getMainHandStack().isOf(Items.CROSSBOW)
-            || entity.getMainHandStack().isOf(Items.GOLDEN_APPLE)
-            || entity.getMainHandStack().isOf(Items.BOW);
+        return entity.getMainHandStack().getMaxUseTime() != 0 //any time greater than 0 has some sort of animation
+            && !entity.getMainHandStack().isOf(Items.SHIELD); //leave out the shield
     }
-
-
-    /**
-     * This boolean doesn't actually return the accurate state of a player shielding or not
-     * However the LivingEntity.isBlocking() method is also inaccurate due to the client not recording the player's itemUseTime correctly
-     */
-    boolean usingShield(LivingEntity entity)
-    {
-        return entity.isUsingItem() && isHoldingShield(entity);
-    }
-
 }
--- src/main/java/walksy/shield/mixin/ModelPredicateProviderRegistryMixin.java
+++ src/main/java/walksy/shield/mixin/ModelPredicateProviderRegistryMixin.java
@@ -5,7 +5,8 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.item.ClampedModelPredicateProvider;
 import net.minecraft.client.item.ModelPredicateProvider;
 import net.minecraft.client.item.ModelPredicateProviderRegistry;
-import net.minecraft.item.*;
+import net.minecraft.item.Item;
+import net.minecraft.item.Items;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -13,15 +14,13 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import walksy.shield.main.ShieldFixMod;
-import walksy.shield.manager.ShieldingPlayer;
 
 import java.util.Map;
 
 @Mixin(ModelPredicateProviderRegistry.class)
-public abstract class ModelPredicateProviderRegistryMixin {
+public class ModelPredicateProviderRegistryMixin {
 
     @Shadow
     @Final
@@ -32,42 +31,17 @@ public abstract class ModelPredicateProviderRegistryMixin {
         if (item == Items.SHIELD) {
             ci.cancel();
             overrideRegisterMethod(Items.SHIELD, new Identifier("blocking"), (stack, world, entity, seed) -> {
-                    if (entity != null) {
-                        if (entity == MinecraftClient.getInstance().player && entity.isUsingItem()) {
-                            if (entity.getActiveItem() == stack) {
-                                return 1.0F;
-                            }
-                        } else {
-                            for (ShieldingPlayer shieldingPlayer : ShieldFixMod.getShieldingManager().shieldingPlayers) {
-                                if (shieldingPlayer.getPlayer() == entity && shieldingPlayer.actuallyShielding()) {
-                                    return 1.0F;
-                                }
-                            }
+                if (entity != null) {
+                    if (entity == MinecraftClient.getInstance().player && entity.isUsingItem()) {
+                        if (entity.getActiveItem() == stack) {
+                            return 1.0F;
                         }
                     } else {
-                        return 0.0F;
-                    }
-                    return 0.0F;
-                });
-
-                /*
-                if (entity == MinecraftClient.getInstance().player)
-                {
-                    if (entity.isUsingItem())
-                    {
-                        return 1.0F;
-                    } else {
-                        return 0.0F;
-                    }
-                }
-                for (ShieldingPlayer shieldingPlayer : ShieldFixMod.getShieldingManager().shieldingPlayers) {
-                    if (shieldingPlayer.actuallyShielding()) {
-                        return 1.0F;
+                        return entity.isUsingItem() && entity.getActiveItem() == stack && entity.isBlocking() ? 1.0F : 0.0F;
                     }
                 }
                 return 0.0F;
-
-                 */
+            });
         }
     }
 
